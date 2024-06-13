c22ef5d9850701068e7acae605456382aa5c3965
beratkrmn7 <125870891+beratkrmn7@users.noreply.github.com>
2024-06-13 19:49:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import static javax.management.Query.times;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int secim;
+        java.lang.String menu = "1- TOPLAMA\n"
+                + "2- ÇIKARMA\n"
+                + "3- ÇARPMA\n"
+                + "4- BÖLME\n"
+                + "5- ÜS ALMA\n"
+                + "6- KÖK ALMA\n"
+                + "7- FAKTORİYEL\n"
+                + "8- ÇIKIŞ YAP";
+        System.out.println(menu);
+        do {
+            System.out.print("Lütfen bir işlem seçiniz :");
+            secim = scan.nextInt();
+            switch (secim) {
+                case 1:
+                    HesapMakinesi.toplama();
+                    break;
+                case 2:
+                    HesapMakinesi.cıkarma();
+                    break;
+                case 3:
+                    HesapMakinesi.carpma();
+                    break;
+                case 4:
+                    HesapMakinesi.bölme();
+                    break;
+                case 5:
+                    HesapMakinesi.üsAlma();
+                    break;
+                case 6:
+                    HesapMakinesi.kökAlma();
+                    break;
+                case 7:
+                    HesapMakinesi.faktoriyel();
+                    break;
+                case 8:
+                    System.out.println("Çıkış yapılıyor!..");
+                    break;
+                default:
+                    System.out.println("Yanlış bir değer girdiniz, tekrar deneyiniz.");
+            }
+        } while (secim != 8);
+
+
     }
 }
